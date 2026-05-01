36ef8c0cbce3e3181055cacfe0526c0380358839
W-SDSS <wakanai027@deltalearns.ca>
2026-05-01 08:59:54 -0700

    w

--- task3.py
+++ task3.py
@@ -1,23 +1,22 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 
 window = tk.Tk()
 window.title("Example")
-window.geometry("340x140")
+window.geometry("260x135")
 window.attributes("-topmost", True)
 window.configure(bg="white")
 
 dogphoto = PhotoImage(file="dog.png")
-label1 = tk.Label(window, image=dogphoto, relief=FLAT)
+f = tk.Frame(window, bg="white")
+label1 = tk.Label(f, image=dogphoto, relief=FLAT)
+label2 = tk.Label(f, text="Pochacco!", bg="white")
+f.pack()
+label1.pack(side=LEFT)
+label2.pack(side=LEFT)
 
-label2 = tk.Label(window, text="Pochacco!", bg="white")
-label3 = tk.Label(window, bg="aqua", font=3, text="A cuddly little puppy! This is from the same\ncreators who brought you Keropi and Kero Kero")
+label3 = tk.Label(window, bg="aqua", text="A cuddly little puppy! This is from the same\ncreators who brought you Keropi and Kero Kero")
+label3.pack()
 
-label1.grid(row=1, column=1)
-label2.grid(row=1, column=1)
-label3.grid(row=2, column=1)
 
 window.mainloop()
-
-#dekinaiiiii
